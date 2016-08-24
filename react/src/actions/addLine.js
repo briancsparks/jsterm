01ef8280d3c9a61402f98b155f9db4cc40e7e77b
Brian C Sparks <brian.c.sparks@hp.com>
2016-08-24 10:33:14 -0700

    Added addLine action

---
+++ react/src/actions/addLine.js
@@ -0,0 +1,5 @@
+import {ADD_LINE} from './const';
+
+module.exports = function(parameter) {
+  return { type: ADD_LINE, parameter };
+};
--- react/src/actions/const.js
+++ react/src/actions/const.js
@@ -1,1 +1,2 @@
 /* Populated by react-webpack-redux:action */
+export const ADD_LINE = 'ADD_LINE';
